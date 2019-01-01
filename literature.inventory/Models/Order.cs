14dc0ee7b569fe69eab412d1e56093125cf901f6
Karl Morosiuk <karl@morosi.uk>
2019-01-01 16:19:00 +0000

    update Order fields to use bson elements

--- literature.inventory/Models/Order.cs
+++ literature.inventory/Models/Order.cs
@@ -1,17 +1,24 @@
-﻿using literature.inventory.Models.Publications;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
+using literature.inventory.Models.Publications;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace literature.inventory.Models
 {
   public class Order
   {
-    public int Id { get; set; }
+    public ObjectId Id { get; set; }
+
+    [BsonElement("Publication")]
     public IPublication Publication { get; set; }
+
+    [BsonElement("Quantity")]
     public int Quantity { get; set; }
+
+    [BsonElement("Publisher")]
     public Publisher Publisher { get; set; }
+
+    [BsonElement("DateOrdered")]
     public DateTime Date { get; set; }
 
   }
